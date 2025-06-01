851a40c2165b25b34d3bcb878dad5463ba8b7b32
RimAaloi <aaloirim@example.com>
2025-06-01 15:15:26 +0200

    update

--- web-digital-banking/src/app/app.component.ts
+++ web-digital-banking/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {AuthService} from './services/auth.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- web-digital-banking/src/app/app.config.ts
+++ web-digital-banking/src/app/app.config.ts
@@ -1,18 +1,12 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-
-import {routes} from './app.routes';
-import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
+import { routes } from './app.routes';
 import {appHttpInterceptor} from './interceptors/app-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes), provideClientHydration(withEventReplay()),  provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(ReactiveFormsModule),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([appHttpInterceptor]))
   ]
 };
